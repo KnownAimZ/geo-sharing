40bd99a500d2807506b2f2a270d2386523285134
dev <dev@gmail.com>
2023-01-08 16:09:04 +0200

    Added: update and delete for geotags.

--- backend/app/Http/Controllers/GeotagController.php
+++ backend/app/Http/Controllers/GeotagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Requests\DestroyGeoTagRequest;
 use App\Http\Requests\GeoTagRequest;
 use App\Http\Resources\GeoTagResource;
 use App\Models\User;
@@ -40,6 +41,44 @@ class GeotagController extends ApiController
 
         return $this->response([
             'message' => __('Geotag was created successfully.'),
+        ]);
+    }
+
+    /**
+     * Update geotag.
+     *
+     * @param  GeoTagRequest  $request
+     * @return JsonResponse
+     */
+    public function update(GeoTagRequest $request): JsonResponse
+    {
+        /** @var User $user */
+        $user = $request->user();
+
+        $user->geoTags()->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'location' => $request->location
+        ]);
+
+        return $this->response([
+            'message' => __('Geotag was successfully updated.'),
+        ]);
+    }
+
+    /**
+     * Delete geotags.
+     *
+     * @param  DestroyGeoTagRequest  $request
+     * @return JsonResponse
+     */
+    public function destroy(DestroyGeoTagRequest $request): JsonResponse
+    {
+        $request->user()->geoTags()->where('id', $request->geotag_id)->delete();
+
+        return $this->response([
+            'geotags' => GeoTagResource::collection($request->user()->geoTags()->get()),
+            'message' => __('Geotag was successfully deleted.'),
         ]);
     }
 }
